19c687012425e8c7592888799a55f949348a644b
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-05-26 04:00:06 +0000

    made changes to free_stack.c

--- free_stack.c
+++ free_stack.c
@@ -30,3 +30,46 @@ void sub_err(stack_t **stack, unsigned int line_number)
 	FREE_STACK(stack);
 	exit(EXIT_FAILURE);
 }
+/**
+ * _getline - get the string from the file stream
+ *  @lineptr: a pointer to an allocated memory
+ *  @n: total byte to be fetched from the stream
+ *  @stream: the file stream to fetch strings
+ *  Return: -1 on fialure, else return total lenght of string fetched
+ */
+
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
+{
+	char *buffer = NULL;
+	ssize_t len = 0;
+	ssize_t size = 0;
+	int c;
+
+	if (!lineptr || !n || !stream)
+	{
+		return (-1);
+	}
+	*lineptr = NULL;
+	while ((c = fgetc(stream)) != EOF)
+	{
+		if (len + 1 >= size)
+		{
+			size = size ? size * 2 : MAX_LINE_LENGTH;
+			buffer = realloc(buffer, size * sizeof(char));
+			if (!buffer)
+			{
+				return (-1);
+			}
+			*lineptr = buffer;
+			*n = size;
+		}
+		buffer[len++] = c;
+		if (c == '\n')
+			break;
+	}
+	if (len == 0)
+		return (-1);
+
+	buffer[len] = '\0';
+	return (len);
+}
